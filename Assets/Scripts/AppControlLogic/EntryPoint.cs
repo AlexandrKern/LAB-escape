38a6f9dc94b4b93a077b09edb6f79bc6f616ced5
JhoJho19 <eugb46@gmail.com>
2024-08-06 22:18:26 +0300

    Architect and EntryPoint

---
+++ Assets/Scripts/AppControlLogic/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EntryPoint : MonoBehaviour
+{
+    SceneLoader sceneLoader = new SceneLoader();
+
+    private void Start()
+    {
+        StartButton.StartButtonPushed.AddListener(GameStart);
+    }
+
+    public void GameStart()
+    {
+        sceneLoader.LoadSceneAsync("Game_Scene");
+    }
+}
