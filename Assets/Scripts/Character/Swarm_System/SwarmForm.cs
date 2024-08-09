46a7dbda33561ace975e0b6763ad744482b94669
Pavel <pedaracha@gmail.com>
2024-08-09 15:05:36 +0300

    try fix scaled swarm

--- Assets/Scripts/Character/Swarm_System/SwarmForm.cs
+++ Assets/Scripts/Character/Swarm_System/SwarmForm.cs
@@ -41,7 +41,8 @@ public class SwarmForm : SwarmFormBase
                     validPoint = true;
                     Transform t = new GameObject().transform;
                     t.parent = transform;
-                    t.position = point;
+                    t.position = point;// (point - (Vector2)spriteRenderer.bounds.min) * transform.lossyScale 
+                       // + (Vector2)spriteRenderer.bounds.min;
                     _points.Add(new DestinationPoint(t, true));
                 }
             }
@@ -49,18 +50,23 @@ public class SwarmForm : SwarmFormBase
     }
 
     Vector2 GetRandomPointInSprite()
-    {
+    {/*
         // Получаем случайную точку в пределах размеров спрайта
         float x = Random.Range(0, spriteRenderer.sprite.bounds.size.x);
         float y = Random.Range(0, spriteRenderer.sprite.bounds.size.y);
 
-        return new Vector2(x, y) + (Vector2)spriteRenderer.bounds.min;
+        return new Vector2(x, y) + (Vector2)spriteRenderer.bounds.min;*/
+        float x = Random.Range(spriteRenderer.bounds.min.x, spriteRenderer.sprite.bounds.max.x);
+        float y = Random.Range(spriteRenderer.bounds.min.y, spriteRenderer.sprite.bounds.max.y);
+
+        return new Vector2(x, y);
     }
 
     bool IsPointVisible(Vector2 point)
     {
         // Преобразуем мировые координаты в пиксельные координаты текстуры
-        Vector2 localPoint = (point - (Vector2)spriteRenderer.bounds.min) / spriteRenderer.bounds.size;
+        //Vector2 localPoint = (point - (Vector2)spriteRenderer.bounds.min) / spriteRenderer.bounds.size;
+        Vector2 localPoint = (point - (Vector2)spriteRenderer.bounds.min) / (spriteRenderer.bounds.max - spriteRenderer.bounds.min);
         localPoint.x *= texture.width;
         localPoint.y *= texture.height;
 
