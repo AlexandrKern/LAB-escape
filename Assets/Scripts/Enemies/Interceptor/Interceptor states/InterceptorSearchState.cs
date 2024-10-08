ef97247c05540ba3e2c47937196a877fead2b4a0
AlexandrKern <137929102+AlexandrKern@users.noreply.github.com>
2024-10-08 18:13:51 +0300

    fixed the behavior when searching for an interceptor

--- Assets/Scripts/Enemies/Interceptor/Interceptor states/InterceptorSearchState.cs
+++ Assets/Scripts/Enemies/Interceptor/Interceptor states/InterceptorSearchState.cs
@@ -5,6 +5,7 @@ public class InterceptorSearchState : IEnemyState
     private Interceptor _interceptor;
     private float _detectionTimer;
     private float _searchTime;
+    private float _maxSearchTime;
 
     public InterceptorSearchState(Interceptor interceptor)
     {
@@ -16,6 +17,7 @@ public class InterceptorSearchState : IEnemyState
         _interceptor.movement.camePlaceOfSearch = false;
         _searchTime = 0;
         _detectionTimer = 0;
+        _maxSearchTime = 0;
     }
 
     public void Execute()
@@ -23,6 +25,7 @@ public class InterceptorSearchState : IEnemyState
         SearchPlayer();
         if (_interceptor.eye.DetectPlayer())
         {
+            Debug.Log("Увидел " );
             _detectionTimer += Time.deltaTime;
             if (_detectionTimer >= _interceptor.eye.detectionTimer)
             {
@@ -32,8 +35,17 @@ public class InterceptorSearchState : IEnemyState
         else if (_interceptor.movement.camePlaceOfSearch)
         {
             _searchTime += Time.deltaTime;
-            Debug.Log(_searchTime);
+            Debug.Log("Время поиска " + _searchTime);
             if (_searchTime >= _interceptor.movement.searchTime)
+            {
+                _interceptor.ChangeState(new InterceptorPatrolState(_interceptor));
+            }
+        }
+        else
+        {
+            _maxSearchTime += Time.deltaTime;
+            Debug.Log("Максимальное время поиска " + _maxSearchTime);
+            if (_maxSearchTime >= 20)
             {
                 _interceptor.ChangeState(new InterceptorPatrolState(_interceptor));
             }
